71a8e2ccf200498db24549eb9952d141bee1b598
Miguel <miguelvm1124@gmail.com>
2026-04-30 11:38:26 +0200

    actualizacion gestion_atletas con implementacion con manejador_archivos

--- src/servicios/gestion_atletas.py
+++ src/servicios/gestion_atletas.py
@@ -1,9 +1,10 @@
+import os
 from src.entidades.deportista import Deportista
 from src.entidades.mediapersonal import MediaCorporal
 from src.entidades.objetivo import Objetivo
 from src.entidades.recordpersonal import RecordPersonal
 from src.entidades.ejercicio import Ejercicio
-
+from src.persistencia.manejador_archivos import ManejadorArchivos
 
 class GestionAtletas:
     """Servicio para gestionar el perfil, metas y récords del usuario"""
@@ -14,25 +15,43 @@ class GestionAtletas:
         self._metas = []
         self._records = []
 
+
+    def cargar_estado(self):
+        """Carga los datos guardados al iniciar el programa"""
+        datos = ManejadorArchivos.cargar_binario("data/atletas.pkl")
+        if datos:
+            self._atletas = datos
+
+    def guardar_estado(self):
+        """Guarda los datos antes de cerrar el programa"""
+        os.makedirs("data", exist_ok=True)
+        ManejadorArchivos.guardar_binario("data/atletas.pkl", self._atletas)
+
+    # --- TU CÓDIGO ORIGINAL (Intacto) ---
     def registrar_atleta(self, nombre: str, peso: float, altura: float) -> Deportista:
-        """Crea un deportista y lo guarda en el sistema"""
         nuevo = Deportista(nombre, peso, altura)
         self._atletas.append(nuevo)
         return nuevo
 
     def añadir_medida(self, medida: MediaCorporal) -> None:
-        """Registra una nueva medición corporal"""
         self._historial_medidas.append(medida)
 
     def fijar_objetivo(self, meta: Objetivo) -> None:
-        """Guarda un nuevo objetivo físico"""
         self._metas.append(meta)
 
     def actualizar_record(self, ej: Ejercicio, marca: float) -> None:
-        """Crea o actualiza un récord personal para un ejercicio"""
         nuevo_record = RecordPersonal(ej, marca)
         self._records.append(nuevo_record)
 
     def obtener_resumen_atleta(self) -> str:
-        """Devuelve un resumen de progreso del usuario"""
         return f"Metas activas: {len(self._metas)} | Récords: {len(self._records)}"
+
+
+    def obtener_todos(self) -> list:
+        return self._atletas
+
+    def calcular_imc_atleta(self, atleta: Deportista) -> float:
+        """Calcula el Índice de Masa Corporal"""
+        if atleta._altura > 0:
+            return round(atleta._peso / (atleta._altura ** 2), 2)
+        return 0.0
